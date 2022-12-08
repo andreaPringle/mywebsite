9ce7d5a1423c9409a486b34468ad53cfcecf17a4
Andrea Pringle <andrea18pringle@gmail.com>
2022-12-08 03:36:55 -0500

    update
    update

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,6 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-  <footer>  
-@Copyright JavaTpoint 2020- All Right Reserved.   
-</footer> 
-</body>
+<div id="foot">
+  <p> © 2022 Copyright: Lavern Busquets 2022</p>
+</div>
+<script src= "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"> </script>
+  <script> src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" </script>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -5,27 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lavern Busquets</title>
+    <link rel="stylesheet" href="css/mysitestyle.css" >
     <link rel="icon" type="image/x-icon" href="img/Busquets__2_-removebg-preview.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link href="css/site.css" rel="stylesheet">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href=" https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> 
+  
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">
-    <img src="img/Busquets__1_-removebg-preview.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">    
-    Lavern Busquets</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        <a class="nav-link" href="menu.php">Menu</a>
-        <a class="nav-link" href="reservation.php">Reservation</a>
-      </div>
-    </div>
-  </div>
+    <header>
+    <div id= "navbarstyled">
+<nav class="navbar navbar-expand-lg ">
+<div class="container-fluid"><a class="navbar-brand" href="index.php">Lavern Busquets</a>
+<button data-toggle="collapse" 
+class="navbar-toggler" data-target="#navcol-1">
+<span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav text-right text-white ml-auto">
+                    <li><a class="nav-link" href="#">About us</a></li>
+                    <li><a class="nav-link" href="#">Gallery</a></li>
+                    <li><a class="nav-link" href="#">Menu</a></li>
+                    <li><a class="nav-link" href="#">Location</a></li>
+                  
+                </ul>
+        
+                <a href="reservation.php" ><button class="btn">Make a Reservation</button></a>
+            </div>
+        </div>
 </nav>
+
+    </div>
+    </header>
